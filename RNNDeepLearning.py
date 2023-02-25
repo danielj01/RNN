69e2cbb204541f5eb3c6d62835d696ffb4ce22e3
danielj01 <123047577+danielj01@users.noreply.github.com>
2023-02-25 10:12:51 -0800

    Add files via upload

---
+++ RNNDeepLearning.py
@@ -0,0 +1,78 @@
+from keras.preprocessing import sequence
+from keras.utils import pad_sequences
+import keras
+import tensorflow as tf
+import os
+import numpy as np
+
+path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+text = open(path_to_file, "rb").read().decode(encoding='utf-8')
+vocab = sorted(set(text))
+# Create a mapping from unique charachters to indices
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+def text_to_int(text):
+  return np.array([char2idx[c] for c in text])
+
+text_as_int = text_to_int(text)
+def int_to_text(ints):
+  try:
+    ints = ints.numpy()
+  except:
+    pass
+  return ''.join(idx2char[ints])
+seq_length = 100
+examples_per_epoch = len(text)//(seq_length+1)
+
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+def split_input_target(chunk): #for an example hello, the function will do
+  input_text = chunk[:-1]      #hell
+  target_text = chunk[1:]      #ello
+  return input_text, target_text #hell, ello
+dataset = sequences.map(split_input_target) # use a map to apply above function to every entry
+BATCH_SIZE = 64
+VOCAB_SIZE = len(vocab)
+EMBEDDING_DIM = 256
+RNN_UNITS = 1024
+#Buffer size shuffle the dataset
+BUFFER_SIZE = 10000
+
+data = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+      tf.keras.layers.Embedding(vocab_size,
+                                embedding_dim,
+                                batch_input_shape=[batch_size, None]),
+      tf.keras.layers.LSTM(rnn_units,
+                           return_sequences=True,
+                           stateful=True,
+                           recurrent_initializer='glorot_uniform'),
+      tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
+
+model = build_model(VOCAB_SIZE, EMBEDDING_DIM, RNN_UNITS, BATCH_SIZE)
+for input_example_batch, target_example_batch in data.take(1):
+  example_batch_predictions = model(input_example_batch)
+  print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+  #Batch size is 64, sequence length is 100 charachters, vocabulary size is 65
+sampled_indices = tf.random.categorical(pred, num_samples=1)
+
+sampled_indices = np.reshape(sampled_indices, (1, -1))[0]
+predicted_chars = int_to_text(sampled_indices)
+
+predicted_chars
+def loss(labels, logits):
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+model.compile(optimizer='adam', loss=loss)
+# Directory where the checkpoints will be saved
+checkpoint_dir = './training_checkpoints'
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True)
+history = model.fit(data, epochs=50, callbacks=[checkpoint_callback])
